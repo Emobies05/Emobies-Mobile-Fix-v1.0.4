d359781512b663a35c5984c8a7f0d4074863e95b
Emobies <248824152+Emobies05@users.noreply.github.com>
2026-02-27 07:17:07 +0400

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/emobies_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/emobies_app/MainActivity.kt
@@ -1,9 +1,3 @@
-- name: Android 15 & Signing Inject
-        run: |
-          # പാക്കേജ് നെയിം പ്ലേ സ്റ്റോർ ഐഡിയുമായി മാച്ച് ചെയ്യുന്നു
-          mkdir -p android/app/src/main/kotlin/com/nxtbit/emobies_24
-          cat > android/app/src/main/kotlin/com/nxtbit/emobies_24/MainActivity.kt << 'EOF'
-          package com.nxtbit.emobies_24
-          import io.flutter.embedding.android.FlutterFragmentActivity
-          class MainActivity: FlutterFragmentActivity()
-          EOF
+package com.nxtbit.emobies_24
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity()
